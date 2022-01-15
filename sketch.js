dd8760a547747272c6b18982d3dfbe0785b11055
selmayagci <selma00@my.yorku.ca>
2022-01-14 21:06:16 -0800

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var w;
+
+function setup() {
+  createCanvas(640, 360);
+  // Make a Walker object
+  w = new Walker();
+}
+
+function draw() {
+  background(46, 134, 193);
+  // Update and display object
+  w.update();
+  w.display();
+}
+
+function Walker() {
+
+  // Start Walker in center
+  this.pos = createVector(width / 2, height / 2);
+
+  this.update = function() {
+    // Move Walker randomly
+    var vel = createVector(random(-5, 5), random(-5, 5));
+    this.pos.add(vel);
+  }
+
+  this.display = function() {
+    // Draw Walker as circle
+    fill(255);
+    rect(this.pos.x, this.pos.y, 48, 48);
+    stroke(212, 172, 13);
+  }
+}
