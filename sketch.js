e34241d71582916edea472093df3d3c064326ca8
selmayagci <selma00@my.yorku.ca>
2022-01-18 23:30:19 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 var w;
+var j;
 
 function setup() {
   createCanvas(640, 360);
   // Make a Walker object
   w = new Walker();
+  j = new Jackson();
 }
 
 function draw() {
@@ -11,6 +13,9 @@ function draw() {
   // Update and display object
   w.update();
   w.display();
+
+  j.update();
+  j.display();
 }
 
 function Walker() {
@@ -30,5 +35,21 @@ function Walker() {
     rect(this.pos.x, this.pos.y, 48, 48);
     strokeWeight(6);
     stroke(212, 172, 13);
+  }
+}
+
+function Jackson() {
+
+  this.pos = createVector(width / 2, height / 2);
+
+  this.update = function() {
+    var vel = createVector(random(-5, 5), random(-5, 5));
+    this.pos.add(vel);
+  }
+
+  this.display = function() {
+    fill(230, 126, 34);
+    ellipse(this.pos.x, this.pos.y, 48, 48);
+    strokeWeight(6);
   }
 }
